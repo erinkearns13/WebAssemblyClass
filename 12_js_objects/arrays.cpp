c722e80296ccbfeb12cd98f765febe3ce5ac13b7
Training <training@ip-172-31-31-168.us-east-2.compute.internal>
2022-10-25 17:47:32 +0000

    Binding more complex objects (struct, class) – calling c classes from js and use of manual delete with refcounts

---
+++ 12_js_objects/arrays.cpp
@@ -0,0 +1,9 @@
+// write a program that takes in a array of students and 
+// update the ui with max, min, avg across the class for each subject
+// student{
+//     name,
+//     english,
+//     maths,
+//     physics
+// }
+// use the value_array to create bindings.
